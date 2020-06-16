825b2cb71bf681de9dd7603360cc3860d4141f76
HansKu7323 <boy7323@gmail.com>
2020-06-16 15:44:14 +0900

    commit3

--- js/main.js
+++ js/main.js
@@ -11,12 +11,14 @@
     //ctx.fillRect(x, y, width, width);
 
     ctx.fillStyle = 'pink';
+    ctx.strokeStyle = '#f00'
     ctx.fillRect(50, 50, 50, 50);
-    ctx.strokeStyle = '#f00';
-    ctx.lineWidth = 8;
-    // ctx.lineJoin = 'round';
-    ctx.lineJoin = 'bevel';
     ctx.strokeRect(50, 50, 50, 50);
+
+    ctx.fillStyle = 'skyblue';
+    ctx.strokeStyle = '#00b'
+    ctx.fillRect(70, 70, 50, 50);
+    ctx.strokeRect(70, 70, 50, 50);
   }
 
   draw();
