a29c11ceb06269764a4a12205138acd137119b23
HansKu7323 <boy7323@gmail.com>
2020-06-16 19:08:12 +0900

    commit5

--- js/main.js
+++ js/main.js
@@ -9,10 +9,19 @@
     const ctx = canvas.getContext('2d');
     
     // ctx.createLinearGradient(x0, y0, x1, y1);
-    const g = ctx.createLinearGradient(0, 0, canvas.width, 0);//漸變
+    // const g = ctx.createLinearGradient(0, 0, canvas.width, 0);//漸變
+    // const g = ctx.createRadialGradient(
+    //   x0, y0, r0,
+    //   x1, y1, r1
+    // );
+
+    const g = ctx.createRadialGradient(
+      canvas.width/2, canvas.height/2, 50,
+      canvas.width/2+100, canvas.height/2+200, 500,
+    );
 
     g.addColorStop(0, '#f00');//起点の色
-    g.addColorStop(0.3, '#0f0');//間の色
+    g.addColorStop(0.1, '#0f0');//間の色
     g.addColorStop(1, '#00f');//終点の色
 
     ctx.fillStyle = g;
