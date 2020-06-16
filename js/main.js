ff9ce6d479c95a4a346cfa80a292750cdafe941e
HansKu7323 <boy7323@gmail.com>
2020-06-16 19:14:01 +0900

    commit6

--- js/main.js
+++ js/main.js
@@ -7,27 +7,14 @@
       return;
     }
     const ctx = canvas.getContext('2d');
-    
-    // ctx.createLinearGradient(x0, y0, x1, y1);
-    // const g = ctx.createLinearGradient(0, 0, canvas.width, 0);//漸變
-    // const g = ctx.createRadialGradient(
-    //   x0, y0, r0,
-    //   x1, y1, r1
-    // );
 
-    const g = ctx.createRadialGradient(
-      canvas.width/2, canvas.height/2, 50,
-      canvas.width/2+100, canvas.height/2+200, 500,
-    );
+    ctx.shadowOffsetX = 4;
+    ctx.shadowOffsetY = 4;
+    ctx.shadowBlur = 4;
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.3)'
 
-    g.addColorStop(0, '#f00');//起点の色
-    g.addColorStop(0.1, '#0f0');//間の色
-    g.addColorStop(1, '#00f');//終点の色
+    ctx.fillRect(50, 50, 50, 50);
 
-    ctx.fillStyle = g;
-
-    //ctx.fillRect(0, 0, 600, 240);
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 
   draw();
